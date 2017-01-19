c05e73c3b7adf7d5460d5768c3e9591077f7992b
scieslak <scieslak@scieslak.com>
2017-01-19 13:20:21 +0000

    Generate PublicPages controller with scieslak view.

---
+++ app/controllers/public_pages_controller.rb
@@ -0,0 +1,4 @@
+class PublicPagesController < ApplicationController
+  def scieslak
+  end
+end
---
+++ test/controllers/public_pages_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class PublicPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get root_path
+    assert_response :success
+  end
+
+end
