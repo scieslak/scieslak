db471b67cec29ea85dd00328a87c940decbb1dfd
scieslak <scieslak@scieslak.com>
2017-02-08 10:33:29 +0000

    Enable public folder - workaround.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'public_pages#scieslak'
   get 'contact', to: 'public_pages#contact'
+  get 'googlea7932c8cde93bccb.html', to: redirect('/googlea7932c8cde93bccb.html')
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
